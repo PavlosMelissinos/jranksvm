6e24d1138a542b952a7d24c35ef60d0a7b836fa3
pavlos <pavlos@pavlos-Core-2-Desktop>
2015-04-26 19:57:48 +0300

    More minor changes
    RankSVM.java
    *Added some javadoc notation

--- mulan/src/mlpms/RankSVM.java
+++ mulan/src/mlpms/RankSVM.java
@@ -2,10 +2,22 @@ package mlpms;
 
 import weka.core.Instance;
 import weka.core.TechnicalInformation;
+import weka.core.TechnicalInformation.Field;
+import weka.core.TechnicalInformation.Type;
 import mulan.classifier.InvalidDataException;
 import mulan.classifier.MultiLabelLearnerBase;
 import mulan.classifier.MultiLabelOutput;
 import mulan.data.MultiLabelInstances;
+
+/**
+ <!-- globalinfo-start -->
+ <!-- globalinfo-end -->
+ *
+ <!-- technical-bibtex-start -->
+ <!-- technical-bibtex-end -->
+ * @author pavlos
+ *
+ */
 
 @SuppressWarnings("serial")
 public class RankSVM extends MultiLabelLearnerBase{
@@ -13,7 +25,6 @@ public class RankSVM extends MultiLabelLearnerBase{
 	@Override
 	protected void buildInternal(MultiLabelInstances trainingSet)
 			throws Exception {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -23,11 +34,23 @@ public class RankSVM extends MultiLabelLearnerBase{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+    public String globalInfo() {
+        return "Class implementing the RankSVM algorithm." + "\n\n" + "For more information, see\n\n" + getTechnicalInformation().toString();
+    }
+    
 	@Override
 	public TechnicalInformation getTechnicalInformation() {
-		// TODO Auto-generated method stub
-		return null;
+        TechnicalInformation result;
+        		  
+        result = new TechnicalInformation(Type.ARTICLE);
+        result.setValue(Field.AUTHOR, "Elisseeff, Andr{\'e} and Weston, Jason");
+        result.setValue(Field.TITLE, "A kernel method for multi-labelled classification");
+        result.setValue(Field.BOOKTITLE, "Advances in neural information processing systems");
+        result.setValue(Field.PAGES, "681--687");
+        result.setValue(Field.YEAR, "2001");
+
+        return result;
 	}
 
 }
