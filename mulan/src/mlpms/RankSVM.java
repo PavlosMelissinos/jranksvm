248ef075081c553581475117aa295dc2da46ef89
pavlosm <guardianhelm@gmail.com>
2015-05-10 00:30:34 +0300

    code cleanup

--- mulan/src/mlpms/RankSVM.java
+++ mulan/src/mlpms/RankSVM.java
@@ -9,16 +9,13 @@ import mulan.classifier.MultiLabelLearnerBase;
 import mulan.classifier.MultiLabelOutput;
 import mulan.data.MultiLabelInstances;
 
+import org.apache.commons.math3.analysis.function.Power;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.BlockRealMatrix;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.stat.StatUtils;
-import org.apache.commons.math3.analysis.function.Add;
-import org.apache.commons.math3.analysis.function.Power;
-import org.apache.commons.math3.analysis.function.Exp;
 import org.apache.commons.math3.util.FastMath;
 
 import weka.core.Instance;
